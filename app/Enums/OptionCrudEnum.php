ba22af7a2c607b08b50ab9ab42b05c48d7b03d0f
JeanKoval <jeancarloskoval2002@gmail.com>
2022-10-11 21:41:32 -0300

    QDL-14 ajuste no Enum OptionCrud

--- app/Enums/OptionCrudEnum.php
+++ app/Enums/OptionCrudEnum.php
@@ -2,10 +2,10 @@
 
 namespace App\Enums;
 
-enum OptionCrudEnum: int{
-    case Incluir    = 1;
-    case Visualizar = 2;
-    case Editar     = 3;
-    case Inativar   = 4;
-    case Excluir    = 5;
+enum OptionCrudEnum: string{
+    case Incluir    = 'incluir';
+    case Visualizar = 'visualizar';
+    case Editar     = 'editar';
+    case Inativar   = 'inativar';
+    case Excluir    = 'excluir';
 }
--- database/migrations/2022_10_11_184753_create_log_cruds_table.php
+++ database/migrations/2022_10_11_184753_create_log_cruds_table.php
@@ -16,7 +16,14 @@ return new class extends Migration
         Schema::create('log_cruds', function (Blueprint $table) {
             $table->id();
             $table->string('rotina');
-            $table->string('acao');
+            $table->enum('acao', [
+                    'incluir',
+                    'visualizar',
+                    'editar',
+                    'inativar',
+                    'excluir'
+                ]
+            );
             $table->bigInteger('registro_id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
