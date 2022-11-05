a9d86659a7699f4835385ac5aff4fc62cbbf7a73
JeanKoval <jeancarloskoval2002@gmail.com>
2022-11-04 21:02:18 -0300

    QDL-20 ajusts a kref in drawer

--- resources/views/layouts/drawer.blade.php
+++ resources/views/layouts/drawer.blade.php
@@ -18,55 +18,55 @@
             <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                 <!-- Sidebar content here -->
                 <li>
-                    <a href="/">
+                    <a href="{{ route('homePage') }}">
                         <ion-icon name="home-outline"></ion-icon>
                         Home
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::Pergunta->value }}">
+                    <a href="{{ route('perguntaShow') }}">
                         <ion-icon name="help-circle-outline"></ion-icon>
                         Perguntas
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::BaseJuridica->value }}">
+                    <a href="{{ route('baseJuridicaShow') }}">
                         <ion-icon name="reader-outline"></ion-icon>
                         Base Jurídica
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::Capitulo->value }}">
+                    <a href="{{ route('capituloShow') }}">
                         <ion-icon name="reader-outline"></ion-icon>
                         Capitulo
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::Artigo->value }}">
+                    <a href="{{ route('artigoShow') }}">
                         <ion-icon name="reader-outline"></ion-icon>
                         Artigo
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::Paragrafo->value }}">
+                    <a href="{{ route('paragrafoShow') }}">
                         <ion-icon name="reader-outline"></ion-icon>
                         Paragrafo
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::Inciso->value }}">
+                    <a href="{{ route('incisoShow') }}">
                         <ion-icon name="reader-outline"></ion-icon>
                         Inciso
                     </a>
                 </li>
                 <li>
-                    <a href="/{{ \App\Enums\RotinasAplicacaoEnum::Alinea->value }}">
+                    <a href="{{ route('alineaShow') }}">
                         <ion-icon name="reader-outline"></ion-icon>
                         Alinea
                     </a>
                 </li>
                 <li>
-                    <a href="/user">
+                    <a href="#">
                         <ion-icon name="people-outline"></ion-icon>
                         Usuários
                     </a>
