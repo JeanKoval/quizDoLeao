de5fade461fa1e2c83d49a85133f519000af76e5
JeanKoval <jeancarloskoval2002@gmail.com>
2022-11-06 22:13:10 -0300

    QDL-20 ajusted components

--- resources/views/livewire/admin/button-confirm-form.blade.php
+++ resources/views/livewire/admin/button-confirm-form.blade.php
@@ -1,8 +1,8 @@
 <div style="text-align: center;">
 
-    @if( $action == \App\Enums\OptionCrudEnum::Excluir )
+    @if( $action == \App\Enums\OptionCrudEnum::Excluir->value )
         <button type="submit" class="btn btn-outline btn-error">Confirmar Exclusão</button>
-    @else
+    @elseif ($action != \App\Enums\OptionCrudEnum::Visualizar->value )
         <button type="submit" class="btn btn-outline btn-success">Salvar</button>
     @endif
 
--- resources/views/livewire/admin/buttons-crud.blade.php
+++ resources/views/livewire/admin/buttons-crud.blade.php
@@ -1,30 +1,30 @@
 <div>
     @if(in_array('visualizar', $actions))
-        <a href="{{ $route }}/visualizar/{{ $idData }}">
+        <a href="{{ \App\Providers\RouteServiceProvider::HOME_ADMIN }}{{ $route }}/visualizar/{{ $idData }}">
             <button class="btn btn-outline btn-info">visualizar</button>
         </a>
     @endif
 
     @if(in_array('alterar', $actions))
-        <a href="{{ $route }}/alterar/{{ $idData }}">
+        <a href="{{ \App\Providers\RouteServiceProvider::HOME_ADMIN }}{{ $route }}/alterar/{{ $idData }}">
             <button class="btn btn-outline btn-warning">Alterar</button>
         </a>
     @endif
 
     @if(in_array('excluir', $actions))
-    <a href="{{ $route }}/excluir/{{ $idData }}">
+    <a href="{{ \App\Providers\RouteServiceProvider::HOME_ADMIN }}{{ $route }}/excluir/{{ $idData }}">
         <button class="btn btn-outline btn-error">Excluir</button>
     </a>
     @endif
 
     @if(in_array('revisao', $actions))
-        <a href="{{ $route }}/incluir/{{ $idData }}?revisao=1">
+        <a href="{{ \App\Providers\RouteServiceProvider::HOME_ADMIN }}{{ $route }}/incluir/{{ $idData }}?revisao=1">
             <button class="btn btn-outline btn-accent">Revisão</button>
         </a>
     @endif
 
     @if(in_array('inativar', $actions))
-        <a href="{{ $route }}/inativar/{{ $idData }}">
+        <a href="{{ \App\Providers\RouteServiceProvider::HOME_ADMIN }}{{ $route }}/inativar/{{ $idData }}">
             <button class="btn btn-outline btn-error">Inativar</button>
         </a>
     @endif
