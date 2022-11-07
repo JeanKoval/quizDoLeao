5a7f815ff1e9a6c3bbb9aac665081199947a36f5
JeanKoval <jeancarloskoval2002@gmail.com>
2022-11-07 20:02:50 -0300

    QDL-26 ajusted return Generator method getPerguntasShow

--- app/Http/Livewire/Admin/Pergunta/Show.php
+++ app/Http/Livewire/Admin/Pergunta/Show.php
@@ -17,9 +17,7 @@ use Livewire\Component;
 
 class Show extends Component
 {
-    //@params crud
-    public $perguntas = [];
-    
+   
     public function mount(){
         Session::put('breadcrumbs', [
             [
@@ -28,82 +26,12 @@ class Show extends Component
                 'icon' => 'pasta'
             ]
         ]);
-        $this->perguntas = Pergunta::all();
-        foreach($this->perguntas as $pergunta){
-            $tipoRelacao = OptionPerguntaEnum::from($pergunta->tipo_relacao);
-
-            if($tipoRelacao == OptionPerguntaEnum::Paragrafo){
-
-                $pergunta->paragrafo = Paragrafo::findOrFail($pergunta->relacao_id);
-                $pergunta->artigo = Artigo::findOrFail($pergunta->paragrafo->artigo_id);
-                
-                $pergunta->paragrafo = $pergunta->paragrafo->numero . '°';
-
-            }else if($tipoRelacao == OptionPerguntaEnum::Inciso){
-
-                $pergunta->inciso = Inciso::findOrFail($pergunta->relacao_id);
-                $tipoRelacaoInciso = OptionIncisoEnum::from($pergunta->inciso->tipo_relacao);
-
-                if($tipoRelacaoInciso == OptionIncisoEnum::Artigo){
-
-                    $pergunta->artigo = Artigo::findOrFail($pergunta->inciso->artigo_id);
-                    
-                }else if($tipoRelacaoInciso == OptionIncisoEnum::Paragrafo){
-                    
-                    $pergunta->paragrafo = Paragrafo::findOrFail($pergunta->inciso->relacao_id);
-                    $pergunta->artigo = Artigo::findOrFail($pergunta->paragrafo->artigo_id);
-
-                    $pergunta->paragrafo = $pergunta->paragrafo->numero . '°';
-                }
-                $pergunta->inciso = $pergunta->inciso->numeroRomano;
-
-            }else if($tipoRelacao == OptionPerguntaEnum::Alinea){
-
-                $pergunta->alinea = Alinea::findOrFail($pergunta->relacao_id);
-                $tipoRelacaoAlinea = OptionAlineaEnum::from($pergunta->alinea->tipo_relacao);
-
-                if($tipoRelacaoAlinea == OptionAlineaEnum::Artigo){
-
-                    $pergunta->artigo = Artigo::findOrFail($pergunta->alinea->artigo_id);
-
-                }else if($tipoRelacaoAlinea == OptionAlineaEnum::Paragrafo){
-
-                    $pergunta->paragrafo = Paragrafo::findOrFail($pergunta->alinea->relacao_id);
-                    $pergunta->artigo = Artigo::findOrFail($pergunta->paragrafo->artigo_id);
-
-                    $pergunta->paragrafo = $pergunta->paragrafo->numero . '°';
-
-                }else if($tipoRelacaoAlinea == OptionAlineaEnum::Inciso){
-
-                    $pergunta->inciso = Inciso::findOrFail($pergunta->alinea->relacao_id);
-                    $tipoRelacaoInciso = OptionIncisoEnum::from($pergunta->inciso->tipo_relacao);
-    
-                    if($tipoRelacaoInciso == OptionIncisoEnum::Artigo){
-    
-                        $pergunta->artigo = Artigo::findOrFail($pergunta->inciso->artigo_id);
-                        
-                    }else if($tipoRelacaoInciso == OptionIncisoEnum::Paragrafo){
-                        
-                        $pergunta->paragrafo = Paragrafo::findOrFail($pergunta->inciso->relacao_id);
-                        $pergunta->artigo = Artigo::findOrFail($pergunta->paragrafo->artigo_id);
-
-                        $pergunta->paragrafo = $pergunta->paragrafo->numero . '°';
-                    }
-                    $pergunta->inciso = $pergunta->inciso->numeroRomano;
-                }
-                $pergunta->alinea = $pergunta->alinea->letra;
-            }
-            
-            $pergunta->capitulo = Capitulo::findOrFail($pergunta->artigo->capitulo_id);
-            $pergunta->baseJuridica = $pergunta->capitulo->getBaseJuridicaAndAno();
-
-            $pergunta->capitulo = $pergunta->capitulo->numeroRomano;
-            $pergunta->artigo = $pergunta->artigo->numero . '°';
-        }
     }
-
+    
     public function render()
     {
-        return view('livewire.admin.pergunta.show');
+        $perguntas = Pergunta::getPerguntasShow();
+
+        return view('livewire.admin.pergunta.show', compact('perguntas'));
     }
 }
