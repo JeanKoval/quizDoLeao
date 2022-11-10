a566e2f92576b620749753c9f3a95392bd998fa1
JeanKoval <jeancarloskoval2002@gmail.com>
2022-11-10 20:05:15 -0300

    QDL-27 ajusted prefix in web-admin

--- routes/web-admin.php
+++ routes/web-admin.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth')
-    ->prefix('admin')
     ->group(function (){
 
         Route::get('/', \App\Http\Livewire\Admin\Home::class)->name('homePageAdmin');
